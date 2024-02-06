21f5b5e5159d27285326f3f4679131afbb3d8d62
chigivera <chmakha2002@example.com>
2024-02-06 19:50:50 +0100

    optimizing

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -4,12 +4,10 @@ let isConnected = false;
 
 export const connectToDB =  async () => {
     mongoose.set('strictQuery',true);
-    if(!process.env.MONGODB_URL) return console.log('MONGODB_URL not found');
-    if(isConnected) return console.log('Already Connected to MongoDB')
+    if(!process.env.MONGODB_URL) return 
+    if(isConnected) return 
     try {
-        await mongoose.connect(process.env.MONGODB_URL)
-        isConnected = true;
-        console.log('Connected to MongoDB')
+        await mongoose.connect(process.env.MONGODB_URL);        isConnected = true;
     } catch (error) {
         console.log(error)
     }
