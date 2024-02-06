f2be932bbdd1953228187675d98de494093193ad
chigivera <chmakha2002@example.com>
2024-02-06 16:29:25 +0100

    fixing error

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,18 +1,5 @@
-import ProfileHeader from "@/components/shared/ProfileHeader";
-import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
-import { currentUser } from "@clerk/nextjs"
-import { Tabs,TabsContent,TabsTrigger, TabsList } from "@/components/ui/tabs";
-import { redirect } from "next/navigation"
-import { profileTabs } from "@/constants";
-import Image from "next/image";
-import ThreadsTab from "@/components/shared/ThreadsTab";
-import UserCard from "@/components/cards/UserCard";
 const Page = async () => {
-    const user = await currentUser();
-    if (!user) return null;
-
-    const userInfo =  await fetchUser(user.id)
-    if(!userInfo?.onboarded) return redirect('/onboarding')    return (
+    return (
         <section>
             <h1 className="head-text mb-10">
                 Communities
