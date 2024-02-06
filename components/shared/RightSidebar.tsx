1798f8568497dfd44fa41527dfaf74ab64839f71
chigivera <chmakha2002@example.com>
2024-02-06 19:13:57 +0100

    fixing more...

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,10 +1,10 @@
 export default function RightSidebar() {
     return (
         <section className="custom-scrollbar rightsidebar">
-            <div className="felx flex-1 flex-col justify-start">
+            <div className="flex flex-1 flex-col justify-start">
                 <h3 className="text-heading4-medium text-light-1">Suggested Communities</h3>
             </div>
-            <div className="felx flex-1 flex-col justify-start">
+            <div className="flex flex-1 flex-col justify-start">
                 <h3 className="text-heading4-medium text-light-1">Suggested Users</h3>
             </div>
         </section>
